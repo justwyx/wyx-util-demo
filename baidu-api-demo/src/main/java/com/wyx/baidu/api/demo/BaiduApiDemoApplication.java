c378100fe6126782e1bcd37c0973fcda4294752c
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-12-22 19:14:19 +0800

    add

---
+++ baidu-api-demo/src/main/java/com/wyx/baidu/api/demo/BaiduApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.baidu.api.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiduApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaiduApiDemoApplication.class, args);
+	}
+
+}
---
+++ baidu-api-demo/src/test/java/com/wyx/baidu/api/demo/BaiduApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyx.baidu.api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiduApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
